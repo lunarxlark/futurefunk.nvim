f5c9756d1ad4d1435a5fcba7cca3093e3b3df036
lunarxlark <lunarxlark@gmail.com>
2023-01-29 23:30:33 +0900

    feat:add palette

---
+++ lua/futurefunk/palette.lua
@@ -0,0 +1,57 @@
+local palette = {
+  white = {
+    base = "#faeece",
+    shade = "#eaecc2",
+    tint = "#e9e9e9",
+  },
+
+  gray = {
+    base = "#717077",
+    shade = "#4f4e53",
+    tint = "#8d8d92",
+  },
+
+  black = {
+    base = "#282828",
+    shade = "#1c1c1c",
+    tint = "#3e3e3e",
+  },
+
+  night = {
+    base = "#31486f",
+    shade = "#22324e",
+    tint = "#8391a9",
+  },
+
+  blue = {
+    base = "#0673A2",
+    shade = "#055172",
+    tint = "#3F98BF",
+  },
+
+  green = {
+    base = "#06A2A2",
+    shade = "#057272",
+    tint = "#3FBFBF",
+  },
+
+  yellow = {
+    base = "#F9C05D",
+    shade = "#AF8741",
+    tint = "#FBD38D",
+  },
+
+  orange = {
+    base = "#F98C5D",
+    shade = "#AF6241",
+    tint = "#FBAE8D",
+  },
+
+  red = {
+    base = "#F95D5D",
+    shade = "#AF4141",
+    tint = "#FB8D8D",
+  },
+}
+
+return palette
